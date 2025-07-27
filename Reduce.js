cca91dab8213c734243cbcadb8a5a73ec48e430c
amitpal603-ops <ap036776@gmail.com>
2025-07-27 12:33:48 +0530

    complate

--- Reduce.js
+++ Reduce.js
@@ -20,6 +20,58 @@ const t = [true,false,true].reduce((ac,curr) => {
 },0)
 // console.log(t);
 
+// medium level --->>>>
+
+// Q1 count frequecny of elemeent
+const input = ['a','b','a','c'].reduce((acc,curr) => {
+   acc[curr] = (acc[curr] || 0) + 1
+   return acc
+},{})
+// console.log(input);
+
+// Q2  calculate averge age from user list
+
+const input1 = [
+   {name : 'A',age : 20},
+   {name : 'B',age : 30}
+].reduce((acc,curr) => {
+    return  acc + (curr.age/2)
+},0)
+// console.log(input1);
+
+// Q3 toatl income by deportment
+const income = [
+   {dept: "IT",salary : 1000},
+   {dept : 'HR', salary : 800},
+   {dept : 'IT',salary : 1200}
+].reduce((accu,curr) => {
+       accu[curr.dept] =  (accu[curr.dept] || 0) + curr.salary
+       return accu
+},{})
+// console.log(income);
+
+// Q4 
+const city = [
+   {city : "Delhi"},
+   {city : "Mumbai"},
+   {city : "Delhi"},
+].reduce((acuu,curr) => {
+   acuu[curr.city] = (acuu[curr.city] || 0) +1
+   return acuu
+},{})
+// console.log(city);
+
+//Q5
+
+const user = [
+   {name : 'A',age : 19},
+   {name : 'B',age : 17},
+   {name : ' C',age : 20},
+].filter((value) => value.age > 18).map((value) => value.name).reduce((accu,curr) => accu + curr,'')
+// console.log(user);
+
+
+
 
 
 
